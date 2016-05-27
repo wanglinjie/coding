02a3740185dfb9441ae3ad2a481cde157223be0c
wanglinjie <hitwhwlj@163.com>
2016-05-27 14:49:44 +0800

    n th ugly number

--- leetcode/uglynumbertwo.py
+++ leetcode/uglynumbertwo.py
@@ -1,4 +1,6 @@
-# import heapq
+# date:20160527
+
+import heapq
 # import itertools
 # class Solution(object):
 #     def nthSuperUglyNumber(self, n, primes):
@@ -6,12 +8,45 @@
 #         :type n: int
 #         :type primes: List[int]
 #         :rtype: int
+
+
+# Write a program to find the n-th ugly number.
+
+# Ugly numbers are positive numbers whose prime factors only include 2, 3, 5. 
+# For example, 1, 2, 3, 4, 5, 6, 8, 9, 10, 12 is the sequence of the first 10 ugly numbers.
+
+# Note that 1 is typically treated as an ugly number.
 #         """
 #         uglies = [1]
 #         merged = heapq.merge(*map(lambda p: (u*p for u in uglies), primes))
 #         uniqed = (u for u, _ in itertools.groupby(merged))
 #         map(uglies.append, itertools.islice(uniqed, n-1))
 #         return uglies[-1]
+
+class Solution(object):
+    def nthUglyNumber(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        if n <= 0:
+            return []
+        uglys = [1]
+        i = j = k = 0
+        for num in xrange(n - 1):
+            uglys.append(min(uglys[i] * 2, uglys[j] * 3, uglys[k] * 5))
+            if uglys[-1] == uglys[i] * 2:
+                i += 1
+            if uglys[-1] == uglys[j] * 3:
+                j += 1
+            if uglys[-1] == uglys[k] * 5:
+                k += 1
+        return uglys[-1]
+
+so = Solution()
+print so.nthUglyNumber(1311)
+
+
 
 
 # so = Solution()
