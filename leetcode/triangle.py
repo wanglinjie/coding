638bc8e9115879e6debe4bedddc1562ca8b9f64e
wanglinjie <hitwhwlj@163.com>
2016-06-20 19:44:02 +0800

    more efficient

--- leetcode/triangle.py
+++ leetcode/triangle.py
@@ -26,25 +26,36 @@ The minimum path sum from top to bottom is 11 (i.e., 2 + 3 + 5 + 1 = 11).
 动态规划，记录到达当前位置最小值
 可以到达当前位置的是上层相邻位置
         """
-        if not triangle:
-            return 0
-        if len(triangle) == 1:
-            return min(triangle[0])
-        triangle_len = len(triangle)
-        for x in xrange(1, triangle_len):
-            line_len = len(triangle[x])
-            for y in xrange(line_len):
+        # if not triangle:
+        #     return 0
+        # if len(triangle) == 1:
+        #     return min(triangle[0])
+        # triangle_len = len(triangle)
+        # for x in xrange(1, triangle_len):
+        #     line_len = len(triangle[x])
+        #     for y in xrange(line_len):
 
-                if y:
-                    if y >= len(triangle[x-1]):
-                        # 如果当前位置比上层个数多，则只计算当前位置上层左侧元素
-                        triangle[x][y] = triangle[x-1][y-1] + triangle[x][y]
-                    else:
-                        triangle[x][y] = min(triangle[x-1][y-1], triangle[x-1][y]) + triangle[x][y]
-                else:
-                    # 如果当前是列表第一个元素，则只需计算上层第一个元素
-                    triangle[x][y] = triangle[x-1][y] + triangle[x][y]
-        return min(triangle[-1])
+        #         if y:
+        #             if y >= len(triangle[x-1]):
+        #                 # 如果当前位置比上层个数多，则只计算当前位置上层左侧元素
+        #                 triangle[x][y] = triangle[x-1][y-1] + triangle[x][y]
+        #             else:
+        #                 triangle[x][y] = min(triangle[x-1][y-1], triangle[x-1][y]) + triangle[x][y]
+        #         else:
+        #             # 如果当前是列表第一个元素，则只需计算上层第一个元素
+        #             triangle[x][y] = triangle[x-1][y] + triangle[x][y]
+        # return min(triangle[-1])
+
+        '''
+        在discuss中看到的代码
+        思路比我的好多了，从下往上遍历，这样就少了那些if判断
+        
+        '''
+        res = triangle[-1]
+        for i in range(len(triangle) -  2, -1, -1):
+            for j in range(len(triangle[i])):
+                res[j] = triangle[i][j] + min(res[j + 1], res[j])
+        return res[0]
 
 triangle = [[1],[1,4],[6,1,7],[1,6,8,3]]
 so = Solution()
