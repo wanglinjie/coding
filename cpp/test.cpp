a05b5b53898b6e8f766639ed512e1d5a7bc8b62b
wanglinjie <hitwhwlj@163.com>
2016-07-11 19:48:09 +0800

    change code

--- cpp/test.cpp
+++ cpp/test.cpp
@@ -1,26 +1,93 @@
 #include <iostream>
+#include <vector>
+#include <cstdlib>
 #include <string>
+#include <stdexcept>
+#include <csignal>
+#include <unistd.h>
 
 using namespace std;
 
-template <typename T>
-T const& Max(T const& a, T const& b)
+#define PI 3.1415926
+#define MIN(a,b) (((a)<(b))?a:b)
+
+#define concat(a, b) a ## b
+template <class T>
+class Stack {
+private:
+    vector<T> elems;
+
+public:
+    void push(T const&);
+    void pop();
+    T top() const;
+    bool empty() const {
+        return elems.empty();
+    }
+};
+
+template <class T>
+void Stack<T>::push (T const& elem)
 {
-    return a < b ? b : a;
+    elems.push_back(elem);
 }
+
+template <class T>
+void Stack<T>::pop()
+{
+    if (elems.empty())
+    {
+        throw out_of_range("Stack<>::pop():empty stack");
+    }
+    elems.pop_back();
+}
+
+template <class T>
+T Stack<T>::top() const
+{
+    if (elems.empty())
+    {
+        throw out_of_range("Stack<>::top():empty stack");
+    }
+
+    return elems.back();
+}
+
+void signalHandler(int signum)
+{
+    cout<<"Interrupt signal ("<<signum<<") received."<<endl;
+    exit(signum);
+}
+
 int main()
 {
-    int i = 39;
-    int j = 20;
-    cout<<"Max(i, j):"<<Max(i, j)<<endl;
+    /*try {
+        Stack<int> intStack;
+        Stack<string> stringStack;
 
-    double f1 = 13.5;
-    double f2 = 20.7;
-    cout<<"Max(f1, f2):"<<Max(f1, f2)<<endl;
+        intStack.push(7);
+        cout<<intStack.top()<<endl;
 
-    string s1 = "Hello";
-    string s2 = "World";
-    cout<<"Max(s1, s2):"<<Max(s1, s2)<<endl;
-
+        stringStack.push("hello");
+        cout<<stringStack.top()<<endl;
+        stringStack.pop();
+        // stringStack.pop();
+    }
+    catch (exception const& ex)
+    {
+        cout<<"Exception: "<<ex.what()<<endl;
+        return -1;
+    }
+    cout<<PI<<endl;
+    cout<<MIN(3, 5)<<endl;
+    int xy = 100;
+    cout<<concat(x, y)<<endl;
+    cout<<__LINE__<<endl;*/
+    signal(SIGINT, signalHandler);
+    while(1)
+    {
+        cout<<"Going to sleep..."<<endl;
+        sleep(1);
+    }
     return 0;
 }
