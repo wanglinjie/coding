bd6f05c523682d6036391ee5314a7963c6902ccb
wanglinjie <hitwhwlj@163.com>
2016-07-28 19:36:22 +0800

    105

---
+++ leetcode/105.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+class Solution {
+public:
+    TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
+        
+    }
+};
+
+int main()
+{
+    return 0;
+}
