d2f1c90738230602fe927c8e34ff8361b8e38356
wanglinjie <hitwhwlj@163.com>
2016-08-03 21:19:48 +0800

    decode ways

---
+++ leetcode/decodeways.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+class Solution {
+public:
+    int numDecodings(string s) {
+        return 0; 
+    }
+};
+
+int main()
+{
+    string s = "";
+    Solution so;
+    int result;
+    result = so.numDecodings(s);
+    cout<<result<<endl;
+}
